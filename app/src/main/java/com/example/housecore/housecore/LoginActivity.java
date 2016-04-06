5552a77300b8dae9359aa766458566188e40f0ed
Tim Kim <timothykimothy@gmail.com>
2016-04-06 13:21:53 -0400

    edads

--- app/src/main/java/com/example/housecore/housecore/LoginActivity.java
+++ app/src/main/java/com/example/housecore/housecore/LoginActivity.java
@@ -37,20 +37,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+public class LoginActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,10 +45,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-     /* Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
+    /* Attempts to sign in or register the account specified by the login form.
+    * If there are form errors (invalid email, missing fields, etc.), the
+    * errors are presented and no actual login attempt is made.
+    */
     private void attemptLogin() {
     }
 
@@ -71,9 +58,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
+        return false;
     }
-
 }
-
